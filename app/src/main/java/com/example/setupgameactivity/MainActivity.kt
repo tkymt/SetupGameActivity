4623d4dd3d52b900150f092646542585c4b9bde8
Taku Yamamoto <jts0ymt@outlook.jp>
2023-12-23 21:17:47 +0900

    GameActivityを継承元に変更した 初期化のコードを書いた システムバーを隠すコードを書いた

--- app/src/main/java/com/example/setupgameactivity/MainActivity.kt
+++ app/src/main/java/com/example/setupgameactivity/MainActivity.kt
@@ -1,10 +1,26 @@
 package com.example.setupgameactivity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import com.google.androidgamesdk.GameActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : GameActivity() {
+    companion object{
+        init {
+            System.loadLibrary("mylibrary")
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        hideSystemBarsForImmersiveMode()
+    }
+
+    private fun hideSystemBarsForImmersiveMode(){
+        val insetsController = WindowCompat.getInsetsController(window, window.decorView)
+        insetsController.hide(WindowInsetsCompat.Type.systemBars())
+        insetsController.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
     }
 }
